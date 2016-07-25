84f1a4d309b48b11c0b5573c38ce7aee7754e4c1
so0323 <so0323@gmail.com>
2016-07-25 16:52:47 +0900

    叩き台

---
+++ source1_w.py
@@ -0,0 +1,32 @@
+﻿import webbrowser
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
+payload = {
+    'loginForm:clientCode': '',
+    'loginForm:loginId': '',
+    'loginForm:password': '',
+    'loginForm:itemkeep': 'true',
+    'loginForm:doLogin.x': '1',
+    'loginForm:doLogin.y': '1',
+    'token': '',
+    'login.php': 'true',
+    'loginForm:hashtoken': '',
+    'loginForm:KinoKbn': '',
+    'loginForm: ScreenWidth': '1280',
+    'loginForm:ScreenHeight': '1024',
+    'loginForm:ClientWidth': '539',
+    'loginForm:ClientHeight': '704'
+}
+
+url = 'https://www3.neppan.net/login.php'
+s = requests.session()
+r = s.get(url)
+soup = BeautifulSoup(r.text,"lxml")
+token = soup.find(attrs={'name': 'token'}).get('value')
+payload['token'] = token
+res = s.post(url, data=payload)
+res2 = requests.get("https://www3.neppan.net/rateGroupDetail.php?mode=update&rateGroupId=1",cookies=s.cookies)
+soup2 = BeautifulSoup(res2.text,"lxml")
+print(soup2.find(attrs={'name': 'rateGroupDetailForm:colItems:1:valueRate_1'}).get('value'))
